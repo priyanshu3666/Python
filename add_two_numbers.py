d36b6df13788cc0a8dc968b36dd9256b19fd049c
Priyanshu Shukla <44258359+priyanshu3666@users.noreply.github.com>
2019-01-10 06:02:46 -0800

    Update add_two_numbers.py

--- add_two_numbers.py
+++ add_two_numbers.py
@@ -2,4 +2,4 @@
 num1 = int(input("Enter 1st number"))
 num2 = int(input("Enter 2nd number"))
 add = num1 + num2
-print "The adition of " , num1 , " + " , num2 , " = " , add
+print("The adition of " , num1 , " + " , num2 , " = " , add)
