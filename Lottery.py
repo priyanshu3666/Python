2cbe97d6fd108b5b23cf6bfa1a395a1c5f2e1db4
Priyanshu Shukla <44258359+priyanshu3666@users.noreply.github.com>
2019-02-25 15:40:31 +0530

    Update Lottery.py

--- Lottery.py
+++ Lottery.py
@@ -1,5 +1,5 @@
 '''A lottery ticket comntains 6 digit .the company has announce that a bumper  prize of rupees 1 lakh will be given to a candidate
-whose ticket number is such that the sum of first three digit is equal  to last 3 digit.
+whose ticket number is such that the sum of first three digit is equal  to last 3 digit.))))))))
 find no. of candidates '''
 
 #PROGRAM STARTED
@@ -8,8 +8,8 @@ for i in range(0,1000000):
     s=str(i)
     if len(s)!=6:
         s="0"*(6-len(s))
-    sum1 = int(s[0])+int(str[1])+int(s[2])
-    sum2 = int(s[3])+int(str[4])+int(s[5])
+    sum1 = int(s[0])+int(s[1])+int(s[2])
+    sum2 = int(s[3])+int(s[4])+int(s[5])
     if sum1==sum2:
         c=c+1
 print("total count",c)        
