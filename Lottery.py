9b768e64151c58c35755b7cf68db2d54a800ce70
Priyanshu Shukla <44258359+priyanshu3666@users.noreply.github.com>
2019-02-25 15:43:42 +0530

    Update Lottery.py

--- Lottery.py
+++ Lottery.py
@@ -7,7 +7,7 @@ c=0
 for i in range(0,1000000):
     s=str(i)
     if len(s)!=6:
-        s="0"*(6-len(s))
+        s="0"*(6-len(s))+s
     sum1 = int(s[0])+int(s[1])+int(s[2])
     sum2 = int(s[3])+int(s[4])+int(s[5])
     if sum1==sum2:
